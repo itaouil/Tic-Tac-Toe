2f05dd154f8b56295dc0015e509073dbf9656ce0
itaouil <itaouil95@gmail.com>
2017-10-08 21:25:42 +0100

    Added click event for when buttons are clicked and small messages showing correct ID assignation.

--- app/src/main/java/itaouil/io/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/itaouil/io/tic_tac_toe/MainActivity.kt
@@ -2,11 +2,43 @@ package itaouil.io.tic_tac_toe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Button click event
+    fun buClick(view: View) {
+
+        // Cast the view to a button view
+        val buSelected:Button = view as Button
+
+        // Cell holder
+        var cellID:Int = 0
+
+        // Assign correct value to cell holder
+        when (buSelected.id) {
+
+            R.id.bu1 -> cellID = 1
+            R.id.bu2 -> cellID = 2
+            R.id.bu3 -> cellID = 3
+            R.id.bu4 -> cellID = 4
+            R.id.bu5 -> cellID = 5
+            R.id.bu6 -> cellID = 6
+            R.id.bu7 -> cellID = 7
+            R.id.bu8 -> cellID = 8
+            R.id.bu9 -> cellID = 9
+
+        }
+
+        // Show message
+        Toast.makeText(this, "ID: " + cellID, Toast.LENGTH_LONG).show()
+
     }
 }
