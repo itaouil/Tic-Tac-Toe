ae61c344681e719a51eee1e6ca20085cac6874ae
itaouil <itaouil95@gmail.com>
2017-10-09 08:42:48 +0100

    Changed layout size, added button selection and winner logic.

--- app/src/main/java/itaouil/io/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/itaouil/io/tic_tac_toe/MainActivity.kt
@@ -1,11 +1,26 @@
 package itaouil.io.tic_tac_toe
 
+// Imports
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
+// Player1 moves
+private var player1 = ArrayList<Int>()
+
+// Player2 moves
+private var player2 = ArrayList<Int>()
+
+// Current player
+private var activePlayer = 1
+
+// Game winner
+private var winner = -1
+
+// Main activity class
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         val buSelected:Button = view as Button
 
         // Cell holder
-        var cellID:Int = 0
+        var cellID = 0
 
         // Assign correct value to cell holder
         when (buSelected.id) {
@@ -37,8 +52,111 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        // Show message
-        Toast.makeText(this, "ID: " + cellID, Toast.LENGTH_LONG).show()
+        // Select the button
+        selectButton(cellID, buSelected)
 
     }
+
+    // Button selection
+    private fun selectButton(cellID:Int, selectedButton:Button) {
+
+        if (activePlayer == 1) {
+
+            // Store player1 move
+            player1.add(cellID)
+
+            // Customise button selected
+            selectedButton.text = "X"
+            selectedButton.setBackgroundColor(Color.GREEN)
+
+            // Change active player
+            activePlayer = 2
+
+        } else {
+
+            // Store player1 move
+            player2.add(cellID)
+
+            // Customise button selected
+            selectedButton.text = "O"
+            selectedButton.setBackgroundColor(Color.YELLOW)
+
+            // Change active player
+            activePlayer = 1
+
+        }
+
+        // Disable button
+        selectedButton.isEnabled = false
+
+        // Check for winner
+        checkWinner()
+
+    }
+
+    // Check for the winner
+    private fun checkWinner() {
+
+        // Row 1
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3))
+            winner = 1
+
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3))
+            winner = 2
+
+        // Row 2
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6))
+            winner = 1
+
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6))
+            winner = 2
+
+        // Row 3
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9))
+            winner = 1
+
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9))
+            winner = 2
+
+        // Column 1
+        if (player1.contains(1) && player1.contains(3) && player1.contains(6))
+            winner = 1
+
+        if (player2.contains(1) && player2.contains(3) && player2.contains(6))
+            winner = 2
+
+        // Column 2
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8))
+            winner = 1
+
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8))
+            winner = 2
+
+        // Column 3
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9))
+            winner = 1
+
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9))
+            winner = 2
+
+        // Diagonal 1
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9))
+            winner = 1
+
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9))
+            winner = 2
+
+        // Diagonal 2
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7))
+            winner = 1
+
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7))
+            winner = 2
+
+        // Winner Message
+        if (winner != -1)
+            Toast.makeText(this, "Player ${winner} won the game !", Toast.LENGTH_LONG).show()
+
+    }
+
 }
